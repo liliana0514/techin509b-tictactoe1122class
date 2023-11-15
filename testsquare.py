5845da5c9dd7016f311e79303e00740d63d1be1e
liliana0514 <148395601+liliana0514@users.noreply.github.com>
2023-11-15 14:21:07 -0800

    Create testsquare.py

---
+++ testsquare.py
@@ -0,0 +1,8 @@
+class TestSquare(unittest.TestCase):
+
+    def test_positives(self):
+        self.assertEqual(square(3), 9)
+        self.assertEqual(square(5), 25)
+
+    def test_negatives(self):
+        self.assertEqual(square(-2), 4)
